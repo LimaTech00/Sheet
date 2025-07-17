750746116cb837ef144c5e40a52d89e95238c744
Lima <lukinha-lenta@hotmail.com>
2025-07-17 18:56:11 -0300

    Incluindo caminho de backup

--- src/main/java/com/example/query/Backup.java
+++ src/main/java/com/example/query/Backup.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class Backup {
 
     public void generateBackup() {
-        String filePath = "";
+        String filePath = "C:\\SomaPDV\\Backup";
         String username = "root";
         String password = "@soma+";
         String databaseName = "db000";
